b275b93f8fe4508ee3f540fe848997c1d850e2c7
ByoungJin Kim <104419151+kevinbj0@users.noreply.github.com>
2025-08-13 15:38:19 +0900

    Update getToken.js

--- api/getToken.js
+++ api/getToken.js
@@ -1,15 +1,16 @@
 export default async function handler(req, res) {
   // 실제 키는 Vercel/Netlify의 안전한 환경 변수 저장소에서 불러옵니다.
+  console.log("테스트1");
   const TOKEN_URL =
     "https://connect-flow-8014--ps.sandbox.my.salesforce.com/services/oauth2/token";
   const CLIENT_ID = "3MVG91x_T0diigV8fsZu5XsZ6laUs2emfHf5SW7tJI46W7Tilwn84t2HRvQPuhXq_s9K.RNM3XQF1NLBFaDgN";
   const CLIENT_SECRET = "89ACA29E4EC15EE072FDC94FE591C016DFCBE5267926543C2447F01988E6623C";
-
+  console.log("테스트2");
   const params = new URLSearchParams();
   params.append("grant_type", "client_credentials");
   params.append("client_id", CLIENT_ID);
   params.append("client_secret", CLIENT_SECRET);
-
+  
   try {
     const apiResponse = await fetch(TOKEN_URL, {
       method: "POST",
